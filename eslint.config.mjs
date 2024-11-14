ff9311bc20824c838e5e1abfb0347ee6acb381bc
shreya-celestialsys <shreya.garg@celestialsys.com>
2024-11-14 14:15:47 +0530

    id-length rule defined

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,17 @@ export default [
    {
        rules: {
            "no-unused-vars": "warn",
-           "no-undef": "warn"
+           "no-undef": "warn",
+
+        // length of variables to be min 2 and max 20 (includes properties of objects as well)
+           "id-length": [
+            "error", {
+                "min": 2,
+                "max": 20,
+                "properties": "always"
+            }
+           ],
+
        }
    }
 ];
