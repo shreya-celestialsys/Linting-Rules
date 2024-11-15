d43f8e87bb9d2a01fef72064fff0126a13c04a19
shreya-celestialsys <shreya.garg@celestialsys.com>
2024-11-15 17:39:08 +0530

    rules for camelcase naming convention and plugin for file structure added

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,37 @@
 import js from "@eslint/js";
+import pluginImport from "eslint-plugin-import";
 
 export default [
     js.configs.recommended,
 
-   {
-       rules: {
-           "no-unused-vars": "warn",
-           "no-undef": "warn",
+    {
+        plugins: {
+			// For file structure
+            import: pluginImport,
+        },
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn",
 
-        // length of variables to be min 2 and max 20 (includes properties of objects as well)
-           "id-length": [
+            // Length of variables to be min 2 and max 20 (includes properties of objects as well)
+            "id-length": [
                 "error", {
                     "min": 2,
                     "max": 20,
                     "properties": "always",
                 }
-           ],
+            ],
 
-        // max number of code lines per file
-           "max-lines": [
+            // Max number of code lines per file
+            "max-lines": [
                 "error", {
                     "max": 200,
                     "skipBlankLines": true,
                 }
-            ]
-       }
-   }
+            ],
+			
+			// For same naming convention throughout the project
+			camelcase: "error"
+        }
+    }
 ];
